380802ecfe8c57bef3bb03bae2c61474751cb5a7
unknown <prashantxworkz@gmail.com>
2022-09-13 15:43:44 +0530

    this is the identity class method with parameters

---
+++ Identity.java
@@ -0,0 +1,59 @@
+class Identity{
+	static void printName(String name){
+		System.out.println("entered printName");
+		System.out.println("Name entered : "+name);
+	}
+	static void printNameAndEmail(String name,String email){
+		System.out.println("entered printNameAndEmai");
+		System.out.println("Name entered : "+name);
+		System.out.println("Email entered : "+email);
+	}
+	static void printNameAndMobile(String name,long mobile){
+		System.out.println("entered printNameAndMobile");
+		System.out.println("Name entered : "+name);
+		System.out.println("Mobile entered : "+mobile);
+	}
+	static void printNameAndAge(String name,byte age){
+		System.out.println("entered printNameAndAge");
+		System.out.println("Name entered : "+name);
+		System.out.println("Age entered : "+age);
+	}
+    static void printNameAndAddress(String name,String address){
+		System.out.println("entered printNameAndAddress");
+		System.out.println("Name entered : "+name);
+		System.out.println("Address entered : "+address);
+	}
+    static void printNameAndEmailAndAddressAndAge(String name,String email,String address,byte age){
+		System.out.println("entered printNameAndEmailAndAddressAndAge");
+		System.out.println("Name entered : "+name);
+		System.out.println("Email entered : "+email);
+		System.out.println("Address entered : "+address);
+		System.out.println("Age entered : "+age);
+	}
+    static void printNameAndGender(String name,char gender){
+		System.out.println("printNameAndGender");
+		System.out.println("Name entered : "+name);
+		System.out.println("Gender entered : "+gender);
+	}
+    static void printNameAndWorking(String name,boolean working){
+		System.out.println("printNameAndWorking");
+		System.out.println("Name entered : "+name);
+		System.out.println("Working entered : "+working);
+	}	
+	static void printNameAndShirtSize(String name,byte shirtSize){
+		System.out.println("printNameAndShirtSize");
+		System.out.println("Name entered : "+name);
+		System.out.println("ShirtSize entered : "+shirtSize);
+	}
+	static void printNameAndPantSize(String name,byte pantSize){
+		System.out.println("printNameAndPantSize");
+		System.out.println("Name entered : "+name);
+		System.out.println("PantSize entered : "+pantSize);
+	}
+	static void printNameAndWeightAndHeight(String name,byte weight,float height){
+		System.out.println("printNameAndWeightAndHeight");
+		System.out.println("Name entered : "+name);
+		System.out.println("Weight entered : "+weight);
+		System.out.println("Height entered : "+height);
+	}
+}
