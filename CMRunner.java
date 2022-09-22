6b3b3f3b877986441ef06c1e58004b1c8dbfd59d
unknown <prashant.xworkz@gmail.com>
2022-09-22 17:49:49 +0530

    pass arguments to main method

---
+++ CMRunner.java
@@ -0,0 +1,30 @@
+class CMRunner{
+	public static void main(String[] values){
+		System.out.println("CM method starts");
+		
+		String ref1=values[0];// bommai
+		System.out.println("index0 : "+ref1);
+		String ref2=values[1];// karnatakcm
+		System.out.println("index1 : "+ref2);
+		String ref3=values[2];// ex-cmson
+		System.out.println("index2 : "+ref3);
+		String ref4=values[3];// bjp
+		System.out.println("index3 : "+ref4);
+		String ref5=values[4];// jd-u
+		System.out.println("index4 : "+ref5);
+		String ref6=values[5];// hm
+		System.out.println("index5 : "+ref6);
+		String ref7=values[6];// BE
+		System.out.println("index6 : "+ref7);
+		String ref8=values[7];// mech
+		System.out.println("index7 : "+ref8);
+		String ref9=values[8];// haveri
+		System.out.println("index8 : "+ref9);
+		String ref10=values[9];// staysInBengaluru
+		System.out.println("index9 : "+ref10);
+		
+		System.out.println("Total values : "+values.length);
+		
+		System.out.println("CM method completed");
+	}
+}
